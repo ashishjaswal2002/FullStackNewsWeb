a3d983bc802d90c137f9197e3757d44b659c60c2
Ashish jaswal <jaswalashish131@gmail.com>
2024-02-16 01:49:37 +0530

    login page added

---
+++ app/Header/header.tsx
@@ -0,0 +1,23 @@
+
+
+export default function TopNotch(){
+
+
+     return (<>
+      
+      <header className='p-8'>
+
+      <div className='flex justify-between'>
+
+     <h1 className='font-medium'>News Web 2.0</h1>
+
+      <div>
+        Dark Mode
+        </div>
+
+      </div>
+      </header>
+
+     
+     </>)
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import LoginPage from "./LoginPage/page";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <LoginPage/>
+    </main>
+  );
+}
